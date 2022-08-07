94052ee7f50f289577ae408a64f3031706a417eb
Lakshya Kumar <lakshyak983@gmail.com>
2022-08-08 02:44:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,131 @@
-import logo from './logo.svg';
-import './App.css';
+import Responses from "./components/Responses";
 
-function App() {
+const App = () => {
+  const DUMMY_DATA = [
+    {
+      id: "1",
+      title: "Lorem Ipsum",
+      responses: [
+        {
+          value: "14",
+        },
+        {
+          value: "67",
+        },
+        {
+          value: "44",
+        },
+      ],
+    },
+    {
+      id: "2",
+      title: "Lorem Ipsum",
+      responses: [
+        {
+          value: "98",
+        },
+        {
+          value: "34",
+        },
+        {
+          value: "57",
+        },
+      ],
+    },
+    {
+      id: "3",
+      title: "Lorem Ipsum",
+      responses: [
+        {
+          value: "88",
+        },
+        {
+          value: "5",
+        },
+        {
+          value: "71",
+        },
+      ],
+    },
+    {
+      id: "4",
+      title: "Lorem Ipsum",
+      responses: [
+        {
+          value: "88",
+        },
+        {
+          value: "5",
+        },
+        {
+          value: "71",
+        },
+      ],
+    },
+    {
+      id: "5",
+      title: "Lorem Ipsum",
+      responses: [
+        {
+          value: "88",
+        },
+        {
+          value: "5",
+        },
+        {
+          value: "71",
+        },
+      ],
+    },
+    {
+      id: "6",
+      title: "Lorem Ipsum",
+      responses: [
+        {
+          value: "88",
+        },
+        {
+          value: "5",
+        },
+        {
+          value: "71",
+        },
+      ],
+    },
+  ];
+
+  const COLORS = [
+    { value: "#4FB1B1", title: "Lorem Ipsum" },
+    { value: "#65CDA7", title: "Lorem Ipsum" },
+    { value: "#D0D0D0", title: "Lorem Ipsum" },
+    { value: "#F78782", title: "Lorem Ipsum" },
+    { value: "#F25264", title: "Lorem Ipsum" },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="container">
+      <h4>Keywords</h4>
+      <div className="wrapper">
+        <div className="responses-wrapper">
+          {DUMMY_DATA.map((data) => (
+            <Responses key={data.id} data={data} />
+          ))}
+        </div>
+        <div className="code-explain">
+          <h4>Codes Explained</h4>
+          {COLORS.map((item) => (
+            <div className="ce-pill">
+              <div
+                className="pill-color"
+                style={{ backgroundColor: `${item.value}` }}
+              ></div>
+              <h6 className="pill-title">{item.title}</h6>
+            </div>
+          ))}
+        </div>
+      </div>
+    </main>
   );
-}
+};
 
 export default App;
---
+++ src/components/Responses/index.jsx
@@ -0,0 +1,68 @@
+import "./responses.css";
+
+const Responses = (props) => {
+  const { data } = props;
+
+  const COLORS = {
+    "0_20": "#F25264",
+    "20_40": "#F78782",
+    "40_60": "#D0D0D0",
+    "60_80": "#65CDA7",
+    "90_100": "#4FB1B1",
+  };
+
+  const buildTileGradient = (intensity) => {
+    if (!intensity) {
+      return `linear-gradient(90deg, #000 50%, #fff)`;
+    }
+    let gradientColor =
+      intensity <= 20
+        ? COLORS["0_20"]
+        : intensity <= 40
+        ? COLORS["20_40"]
+        : intensity <= 60
+        ? COLORS["40_60"]
+        : intensity <= 80
+        ? COLORS["60_80"]
+        : intensity <= 100
+        ? COLORS["90_100"]
+        : "#000";
+    if (intensity >= 21 && intensity <= 40) intensity -= 20;
+    if (intensity >= 41 && intensity <= 60) intensity -= 40;
+    if (intensity >= 61 && intensity <= 80) intensity -= 60;
+    if (intensity >= 81 && intensity <= 100) intensity -= 80;
+    let intensityPercent = (intensity / 20) * 100;
+    return `linear-gradient(90deg, ${gradientColor} ${intensityPercent}%, #fff)`;
+  };
+
+  function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+    //Inclusive of max & min
+  }
+
+  return (
+    <div className="responses">
+      <div className="title">{data.title}</div>
+      <div className="heatmap">
+        {[...Array(5)].map((el, index) => {
+          let randomVal = getRandomIntInclusive(1, 100);
+          return (
+            <span
+              key={index}
+              className="tile"
+              style={{
+                background: buildTileGradient(randomVal),
+              }}
+            >
+              {randomVal || "-"}
+            </span>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Responses;
